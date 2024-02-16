977770ad7dbdf21eb26319f574214f111189d080
lymann <phplaravel576@gmail.com>
2024-02-16 10:04:29 +0700

    first commit

---
+++ BlockCodeExecution.java
@@ -0,0 +1,62 @@
+package thread;
+
+class Define{
+    private void print(String string){
+        for(int i=0;i<string.length();i++){
+            System.out.print(string.charAt(i));
+            try {
+                Thread.sleep(100);
+            }catch (Exception exception){
+                System.out.println(exception.getMessage());
+            }
+        }
+        System.out.println();
+    }
+    public synchronized void printWelcome(String chars){this.print(chars);}
+    public synchronized void printStars(String chars){this.print(chars);}
+    public synchronized void printMsg(String chars){this.print(chars);}
+    public synchronized void printLine(String chars){this.print(chars);}
+}
+
+class Reader1 extends Thread{
+    @Override
+    public void run() {
+
+    }
+}
+
+public class BlockCodeExecution {
+    public static void main(String[] arg){
+        Define define = new Define();
+        String welcome = "Center of Science and Technology Advanced Development";
+        String star = "=".repeat(55);
+        String msg = "No giving up this opportunity, do your best first";
+        String line = "-".repeat(55);
+        Thread thread1 = new Thread(() -> {
+            define.printWelcome(welcome);
+        });
+        Thread thread2 = new Thread(() -> {
+            define.printStars(star);
+        });
+        Thread thread3 = new Thread(() -> {
+            define.printMsg(msg);
+        });
+        Thread thread4 = new Thread(() -> {
+            define.printLine(line);
+        });
+
+        try {
+            thread1.start();
+            thread1.join();
+            thread2.start();
+            thread2.join();
+            thread3.start();
+            thread3.join();
+            thread4.start();
+            thread4.join();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
+
